efaf6e288e79cc592cc212a73cc09e01a7d65d32
Asus <Asus@DESKTOP-LUBP48L>
2023-12-10 15:09:17 +0600

    Добавьте файлы проекта.

---
+++ Module13practice/MusicCatalog.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module13practice
+{
+    public class MusicCatalog
+    {
+        Hashtable catalog = new Hashtable();
+
+        public void AddDisk(string diskTitle)
+        {
+            if (!catalog.ContainsKey(diskTitle))
+            {
+                catalog[diskTitle] = new Hashtable();
+                Console.WriteLine($"Диск '{diskTitle}' добавлен в каталог.");
+            }
+            else
+            {
+                Console.WriteLine($"Диск '{diskTitle}' уже существует в каталоге.");
+            }
+        }
+
+        public void RemoveDisk(string diskTitle)
+        {
+            if (catalog.ContainsKey(diskTitle))
+            {
+                catalog.Remove(diskTitle);
+                Console.WriteLine($"Диск '{diskTitle}' удален из каталога.");
+            }
+            else
+            {
+                Console.WriteLine($"Диск '{diskTitle}' не найден в каталоге.");
+            }
+        }
+
+        public void AddSong(string diskTitle, string songTitle, string artist)
+        {
+            if (catalog.ContainsKey(diskTitle))
+            {
+                Hashtable disk = (Hashtable)catalog[diskTitle];
+                if (!disk.ContainsKey(songTitle))
+                {
+                    disk[songTitle] = artist;
+                    Console.WriteLine($"Песня '{songTitle}' добавлена на диск '{diskTitle}'.");
+                }
+                else
+                {
+                    Console.WriteLine($"Песня '{songTitle}' уже существует на диске '{diskTitle}'.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Диск '{diskTitle}' не найден в каталоге.");
+            }
+        }
+
+        public void RemoveSong(string diskTitle, string songTitle)
+        {
+            if (catalog.ContainsKey(diskTitle))
+            {
+                Hashtable disk = (Hashtable)catalog[diskTitle];
+                if (disk.ContainsKey(songTitle))
+                {
+                    disk.Remove(songTitle);
+                    Console.WriteLine($"Песня '{songTitle}' удален из каталога '{diskTitle}'.");
+                }
+                else
+                {
+                    Console.WriteLine($"Песня '{songTitle}' не найден на диске '{diskTitle}'.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Диск '{diskTitle}' не найден в каталоге.");
+            }
+        }
+
+        public void DisplayCatalog()
+        {
+            Console.WriteLine("Музыкальный каталог:");
+            foreach (string diskTitle in catalog.Keys)
+            {
+                Console.WriteLine($"- Диск: {diskTitle}");
+                Hashtable disk = (Hashtable)catalog[diskTitle];
+                foreach (string songTitle in disk.Keys)
+                {
+                    string artist = (string)disk[songTitle];
+                    Console.WriteLine($"  - Песня: {songTitle}, Автор: {artist}");
+                }
+            }
+        }
+
+        public void SearchByArtist(string artist)
+        {
+            Console.WriteLine($"Результаты поиска для исполнителя '{artist}':");
+            foreach (string diskTitle in catalog.Keys)
+            {
+                Hashtable disk = (Hashtable)catalog[diskTitle];
+                foreach (string songTitle in disk.Keys)
+                {
+                    string songArtist = (string)disk[songTitle];
+                    if (songArtist.Equals(artist, StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine($"- Диск: {diskTitle}, Песня: {songTitle}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Module13practice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module13practice
+{
+    public class Program
+    {
+        static void Main()
+        {
+            MusicCatalog catalog = new MusicCatalog();
+            // Добавление дисков и песен
+            catalog.AddDisk("Диск");
+            catalog.AddSong("Диск1", "Песня1", "Автор1");
+            catalog.AddSong("Диск1", "Песня2", "Автор2");
+            catalog.AddDisk("Диск2");
+            catalog.AddSong("Диск2", "Песня3", "Автор1");
+            catalog.AddSong("Диск2", "Песня4", "Автор3");
+            // Вывод каталога
+            catalog.DisplayCatalog();
+            // Поиск по исполнителю
+            catalog.SearchByArtist("Автор1");
+            Console.ReadKey ();
+        }
+    }
+}
